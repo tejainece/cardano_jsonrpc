e4a2c498588677c4b3af0f35341398afb37b181c
Ravi Teja Gudapati <tejainece@gmail.com>
2021-09-13 19:01:59 +0200

    keys

---
+++ lib/src/wallet/private_key.dart
@@ -0,0 +1,39 @@
+import 'package:ninja_ed25519/ninja_ed25519.dart';
+
+class AdaPrivateKey {
+  final RFC8032Seed key;
+
+  AdaPrivateKey(this.key);
+
+  factory AdaPrivateKey.fromBech32(String key) {
+    return AdaPrivateKey(RFC8032Seed.fromBech32(key));
+  }
+
+  AdaPublicKey get public => AdaPublicKey(key.publicKey);
+
+  String toBech32({String prefix = ''}) {
+    // TODO
+    throw UnimplementedError();
+  }
+}
+
+class AdaPublicKey {
+  final PublicKey key;
+
+  AdaPublicKey(this.key);
+
+  factory AdaPublicKey.fromBech32(String key) {
+    // TODO return AdaPublicKey(PublicKey.fromBech32(key));
+    throw UnimplementedError();
+  }
+
+  String toBech32({String prefix = ''}) {
+    // TODO
+    throw UnimplementedError();
+  }
+
+  String toAddress() {
+    // TODO
+    throw UnimplementedError();
+  }
+}
